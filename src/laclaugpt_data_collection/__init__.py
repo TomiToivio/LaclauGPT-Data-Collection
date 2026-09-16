e4ab687b22993685092df4fa89a0e493495d9db2
Tomi Toivio (ψ/acc) <tomi.toivio@helsinki.fi>
2026-09-16 19:57:03 +0300

    Expose collection plugin API

--- src/laclaugpt_data_collection/__init__.py
+++ src/laclaugpt_data_collection/__init__.py
@@ -9,14 +9,32 @@ from .models import (
     SourceSection,
     canonicalize_source_url,
 )
+from .plugins import (
+    CollectionContext,
+    CollectionPlugin,
+    CollectionRunner,
+    PluginRegistry,
+    PluginRunResult,
+    PluginSpec,
+    adapt_collector,
+    default_registry,
+)
 
 __all__ = [
     "CanonicalRecord",
+    "CollectionContext",
+    "CollectionPlugin",
     "CollectionProvenance",
+    "CollectionRunner",
     "ContentSection",
     "MediaReference",
     "NormalizedRecord",
+    "PluginRegistry",
+    "PluginRunResult",
+    "PluginSpec",
     "SourceSection",
+    "adapt_collector",
     "canonicalize_source_url",
+    "default_registry",
 ]
 __version__ = "0.1.0"
