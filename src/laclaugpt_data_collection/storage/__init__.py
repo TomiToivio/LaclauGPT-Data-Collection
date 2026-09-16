a7c589d12b084d7b6dfc8410ceec20738edfb85a
Tomi Toivio (ψ/acc) <tomi.toivio@helsinki.fi>
2026-09-16 15:14:11 +0300

    feat: export CSV record store

--- src/laclaugpt_data_collection/storage/__init__.py
+++ src/laclaugpt_data_collection/storage/__init__.py
@@ -1,9 +1,11 @@
 """Storage adapters for local and distributed deployments."""
 
 from .base import ObjectStore, RecordStore
+from .csv import CSVRecordStore
 from .local import FilesystemObjectStore, SQLiteRecordStore
 
 __all__ = [
+    "CSVRecordStore",
     "FilesystemObjectStore",
     "ObjectStore",
     "RecordStore",
